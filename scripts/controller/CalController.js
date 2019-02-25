f0a7be288c1748db72a411a769d7e5b3cfa54c21
unknown <brunogfvot@hotmail.com>
2019-02-24 21:59:46 -0300

    DOM

---
+++ scripts/controller/CalController.js
@@ -0,0 +1,76 @@
+class CalController{
+
+    constructor(){
+
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+    }    
+
+    initialize(){
+
+        this.setDiplayDateTime();
+
+        setInterval(()=>{
+
+            this.setDiplayDateTime();
+        }, 1000);
+       
+    }
+
+    setDiplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+            
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+
+    get displayDate(){
+
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+
+        return this._dateEl.innerHTML = value;
+    }
+
+    get displayTime(){
+
+        return this._timeEl.innerHTML;
+    }
+       
+    set displayTime(value){
+
+        return this._timeEl.innerHTML = value;
+    }
+
+    get displayCalc(){
+
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value){
+
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate(){
+
+        return new Date();
+    }
+
+    set currentDate(value){
+
+        this._currentDate = value;
+    }
+
+}
