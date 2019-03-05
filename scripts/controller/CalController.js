3e0fbdfd56315f333f90d3588e7f774cb286fcdb
Bruno Romeo <31778416+brunoromeo001@users.noreply.github.com>
2019-03-05 16:39:35 -0300

    For Procurando o último número do Array
    For Procurando o último número do Array

--- scripts/controller/CalController.js
+++ scripts/controller/CalController.js
@@ -67,12 +67,25 @@ class CalController{
         let result = eval(this._operation.join(""));
 
         this._operation = [result, last];
+
+        this.setLastNumberToDisplay();
     
     }
 
     setLastNumberToDisplay(){
 
-        
+        let lastNumber;
+
+        for(let i = this._operation.length -1; i >= 0; i--){
+
+            if(!this.isOperator(this._operation[i])){
+
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+
+        this.displayCalc = lastNumber;
     }
 
     addOperation(value){
@@ -90,6 +103,8 @@ class CalController{
             } else{
 
                 this.pushOperation(value);
+
+                this.setLastNumberToDisplay();
             }
 
         } else{
@@ -104,9 +119,7 @@ class CalController{
                 this.setLastOperetion(parseInt(newValue));
 
                 this.setLastNumberToDisplay();
-
-            }
-            
+            }            
         }        
         
     }
