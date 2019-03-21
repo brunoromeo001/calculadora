812ebe75e3b0edc5778759f1c0c0f995afca4f63
Bruno Romeo <31778416+brunoromeo001@users.noreply.github.com>
2019-03-21 18:54:17 -0300

    Clicando mais de uma vez no botão igual
    Clicando mais de uma vez no botão igual

--- scripts/controller/CalController.js
+++ scripts/controller/CalController.js
@@ -2,6 +2,8 @@ class CalController{
 
     constructor(){
         
+        this._lastOperator = '';
+        this._lastNumber = '';
         this._operation = [];
         this._locale = "pt-BR";
         this._displayCalcEl = document.querySelector("#display");
@@ -75,17 +77,34 @@ class CalController{
             this.calc();
         }
     }
+    getResult(){
+
+        return eval(this._operation.join(""));
+    }
 
     calc(){
 
         let last= "";
+        
+        this._lastOperator = this.getLastItem();
+
+        if(this._operation.length <3){
+
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
+        }
 
         if(this._operation.length > 3){
 
             last = this._operation.pop();
+            this._lastNumber = this.getResult();
+
+        }else if (this._operation.length == 3){
+            
+            this._lastNumber = this.getLastItem(false);
         }
         
-        let result = eval(this._operation.join(""));
+        let result = this.getResult();
 
         if (last == '%'){
             
@@ -102,18 +121,30 @@ class CalController{
         this.setLastNumberToDisplay();    
     }
 
-    setLastNumberToDisplay(){
+    getLastItem(isOperator = true){
 
-        let lastNumber;
+        let lastItem;
 
         for(let i = this._operation.length -1; i >= 0; i--){
+                
+            if(this.isOperator(this._operation[i]) == isOperator){
 
-            if(!this.isOperator(this._operation[i])){
-
-                lastNumber = this._operation[i];
+                lastItem = this._operation[i];
                 break;
-            }
+            }           
         }
+
+        if (!lastItem){
+
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+        }
+        
+        return lastItem;
+    }
+
+    setLastNumberToDisplay(){
+
+        let lastNumber = this.getLastItem(false);
 
         if(!lastNumber) lastNumber = 0;
 
