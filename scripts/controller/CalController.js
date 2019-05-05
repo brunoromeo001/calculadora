e26f4314c14c365407d3936b7dd90fa12317602a
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2019-05-05 14:44:52 -0300

    Eventos de Teclado

--- scripts/controller/CalController.js
+++ scripts/controller/CalController.js
@@ -12,6 +12,7 @@ class CalController{
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyboard();
     }    
 
     addEventListenerAll(element, events, fn){
@@ -32,6 +33,55 @@ class CalController{
         }, 1000);     
         
         this.setLastNumberToDisplay();
+    }
+
+    initKeyboard(){
+
+        document.addEventListener('keyup', e=>{
+                      
+            switch (e.key){
+
+                case 'Escape':               
+                case 'Delete': 
+                    this.clearAll();
+                break;
+    
+                case 'Backspace':
+                    this.clearEntry();8
+                break;
+    
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                    this.addOperation(e.key);
+                break;    
+                
+                case 'Enter':
+                case '=':
+                    this.calc();
+                break;
+    
+                case '.':
+                case ',':
+                    this.addDot();
+                break;
+    
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    this.addOperation(parseInt(e.key));
+                break;                    
+            }
+        });
     }
 
     setError(){
@@ -211,7 +261,7 @@ class CalController{
             break;
 
             case 'ce':
-                this.clearEntry();
+                this.clearEntry();8
             break;
 
             case 'soma':
